373ff3e523e8766f642abe1f859e99b2ff61b132
Gustavo Adolpho <gumg90@gmail.com>
2020-04-22 21:00:28 -0300

    Automation Test

--- cypress/integration/olxTesting.spec.js
+++ cypress/integration/olxTesting.spec.js
@@ -10,12 +10,12 @@ describe('Evaluation of automation tests', () => {
         Cypress.on('uncaught:exception', () =>{return false});
         cy.visit(pageElements.url)
         searchItem.searchItem()
-      
+    })
     it('display only five ads with name and amount', () => {
         mainPage.displayOnlyFiveItens()
-    });
+    })
 
     it('print first ads', () => {
         mainPage.printFirstItemPage()
-    });
+    })
 });
